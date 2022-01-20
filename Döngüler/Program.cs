fca6c3d5784634fdea6996e912b67e7af8bd577d
Sabahci <Sabahci@DESKTOP-6C2KRTE>
2022-01-20 12:28:20 +0300

    Add project files.

---
+++ Döngüler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Döngüler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=6;
+            
+           
+            
+            for(int i=0; i <=a; i++)
+            {
+                if (i == 5)
+                {
+                    Console.WriteLine("i is 5");
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("end of for");
+        }
+        
+    }
+}
---
+++ whileLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace whileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 100;
+            bool ısPrime = true;
+
+            for (int i = 1; i <=number; i++)
+            {
+                for (int j = 2; j < i-1; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        ısPrime = false;
+                        break;
+                    }
+                }
+                if (ısPrime)
+                {
+                    Console.WriteLine(i);
+                }
+                ısPrime = true;
+            }
+        }
+    }
+}
